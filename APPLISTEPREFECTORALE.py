562d8e0f4df4ff530527be6ecf5864b8edd3e98e
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-09-20 15:30:11 +0200

    Update APPLISTEPREFECTORALE.py

--- APPLISTEPREFECTORALE.py
+++ APPLISTEPREFECTORALE.py
@@ -8,32 +8,31 @@ def load_base_data():
     return pd.read_excel(base_url)
 
 # Fonction pour effectuer la correspondance
-def perform_lookup(input_df, base_df):
-    merged_df = pd.merge(input_df, base_df, how="left", left_on="CODE", right_on="CODE")
-    return merged_df
+def perform_lookup(input_codes, base_df):
+    filter_condition = base_df['CODE'].isin(input_codes)
+    result_df = base_df[filter_condition]
+    return result_df
 
 # Interface Streamlit
 st.title("Application pour correspondance de CODE")
 
-uploaded_file = st.file_uploader("Choisissez un fichier Excel", type=["xlsx"])
+uploaded_file = st.file_uploader("Choisissez un fichier texte avec les codes", type=["txt"])
 
 if uploaded_file:
-    input_df = pd.read_excel(uploaded_file)
-
-    # Nettoyage des données : supprimer les lignes où la colonne 'CODE' est vide
-    input_df.dropna(subset=['CODE'], inplace=True)
+    input_txt = uploaded_file.read().decode("utf-8")
+    input_codes = input_txt.strip().split('\n')
     
     base_df = load_base_data()
     base_df.dropna(subset=['CODE'], inplace=True)
 
     # Spécifiez les colonnes à utiliser
-    base_columns = [0, 4, 5, 8, 18, 11, 13, 14, 21, 16]  # Par exemple, la colonne 0 pour "CODE", la colonne 4 pour "SIRET PREF", etc.
+    base_columns = [0, 4, 5, 8, 18, 11, 13, 14, 21, 16]
 
     # Filtrer les colonnes
     base_df_filtered = base_df.iloc[:, base_columns]
 
     # Effectuer la correspondance
-    result_df = perform_lookup(input_df, base_df_filtered)
+    result_df = perform_lookup(input_codes, base_df_filtered)
 
     # Afficher le résultat
     st.write(result_df)
